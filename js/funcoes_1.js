6e7d4bde7f47652bf27474fac91ab4b0dc29c0ee
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-15 17:24:14 -0300

    Atividade pratica 11 JS

---
+++ js/funcoes_1.js
@@ -0,0 +1,38 @@
+/*
+a. Receba do usuário através do comando prompt um CPF (apenas números), e atribua isso a uma variável.
+
+b. Crie uma função para verificar se o CPF digitado contém 11 dígitos, caso não contenha retorne false true caso retorne.
+
+c. Crie uma função para formatá-lo de acordo com a máscara xxx.xxx.xxx-xx
+
+d. Crie uma última função para exibir o CPF formatado através de um alert para o cliente caso a função de validação de dígitos retorne true.
+
+986.848.766-87
+*/
+
+{
+    function recebeCPFUsuario(cpf) {
+        if (cpf.length == 11) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    let recebeCPF = prompt('Digite seu CPF (apenas números): ');
+    alert(recebeCPFUsuario(recebeCPF));
+
+    function cpfFormatado(cpf) {
+        for (let cpf of cpfFormatado) {
+            if (cpf == true) {
+            let parte1 = recebeCPF.slice(0, 3);
+            let parte2 = recebeCPF.slice(4, 7);
+            let parte3 = recebeCPF.slice(8, 11);
+            let parte4 = recebeCPF.slice(12, 14);
+            return cpf = `${parte1}.${parte2}.${parte3}-${parte4}`;
+            }
+        }
+    }
+    
+    alert(cpfFormatado(cpf));
+
+}
