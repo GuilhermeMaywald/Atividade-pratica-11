48f8a0f6286b72068025ff207452af226b1e2d4d
guilherme maywald <guilhermemaywald@gmail.com>
2022-09-16 18:16:07 -0300

    Resolução completa

--- js/funcoes_1.js
+++ js/funcoes_1.js
@@ -7,7 +7,6 @@ c. Crie uma função para formatá-lo de acordo com a máscara xxx.xxx.xxx-xx
 
 d. Crie uma última função para exibir o CPF formatado através de um alert para o cliente caso a função de validação de dígitos retorne true.
 
-986.848.766-87
 */
 
 {
@@ -19,16 +18,17 @@ d. Crie uma última função para exibir o CPF formatado através de um alert pa
         }
     }
     let recebeCPF = prompt('Digite seu CPF (apenas números): ');
-    alert(recebeCPFUsuario(recebeCPF));
-
-    function cpfFormatado(cpf) {
-        if (cpf == true) {
+    let cpfValido = recebeCPFUsuario(recebeCPF);
+    function cpfFormatado(cpf,cpfVerdadeiro) {
+        if (cpfVerdadeiro == true) {
             let parte1 = cpf.slice(0, 3);
-            let parte2 = cpf.slice(4, 7);
-            let parte3 = cpf.slice(8, 11);
-            let parte4 = cpf.slice(12, 14);
+            let parte2 = cpf.slice(3, 6);
+            let parte3 = cpf.slice(6, 9);
+            let parte4 = cpf.slice(9, 11);
             return cpf = `${parte1}.${parte2}.${parte3}-${parte4}`;
+        } else {
+            alert('Digite um CPF válido!');
         }
     }
-    alert(cpfFormatado(cpf));
+    alert(cpfFormatado(recebeCPF, cpfValido));
 }
